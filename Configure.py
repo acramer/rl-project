f2a155c8c2887101e52ac88fd0c4450f7a2c27dd
acramer <acramer@tamu.edu>
2021-04-28 23:24:01 -0500

    added req.yml config.py and removed bugs from gridword

---
+++ Configure.py
@@ -0,0 +1,103 @@
+
+default_configs = {
+
+    # NOTE: Hyper Parameters (ideally) in order of most used:
+
+    'epochs':            {'default': 1,
+                          'flags':   ['--epochs', '-e'],
+                          'argparse':{'type':int}},
+    'learning_rate':     {'default': 0.001,
+                          'flags':   ['--learning_rate', '-L'],
+                          'argparse':{'type':float}},
+    'batch_size':        {'default': 128,
+                          'flags':   ['--batch_size', '-B'],
+                          'argparse':{'type':int}},
+    'weight_decay':      {'default': 0.0002,
+                          'flags':   ['--weight_decay', '-W'],
+                          'argparse':{'type':float}},
+    'save_interval':     {'default': None,
+                          'flags':   ['--save_int', '-S'],
+                          'argparse':{'type':int,'help':'Save Interval in epochs.  A value of 0 saves at completion only.  All valid interval values save on completion in model.th in specified directory.'}},
+
+    'step_schedule':     {'default': False,
+                          'flags':   ['--step_schedule', '-s'],
+                          'argparse':{'action':'store_true','help':'Learning rate decay.'}},
+    'cosine':            {'default': False,
+                          'flags':   ['--cosine'],
+                          'argparse':{'action':'store_true','help':'CosineAnnealing Learning Rate Scheduler.'}},
+    'adam':              {'default': False,
+                          'flags':   ['--adam', '-a'],
+                          'argparse':{'action':'store_true','help':'Adam optimizer used.'}},
+    'dropout_rate':      {'default': 0.0,
+                          'flags':   ['--dropout_rate', '-D'],
+                          'argparse':{'type':float}},
+
+    'max_steps':         {'default': 20,
+                          'flags':   ['--max_steps', '-E'],
+                          'argparse':{'type':int}},
+
+    # NOTE: Hyper Parameters that change the least:
+
+    # NOTE: Training/Evaluation args that don't effect learning algorithm:
+
+    # NOTE: Example, maybe implement
+    # 'wandb':             {'default': True,
+    #                       'flags':   ['--no_logging', '-n'],
+    #                       'argparse':{'action':'store_false','help':'Turns logging on Weights&Bias off.'}},
+    'description':       {'default': '',
+                          'flags':   ['--des'],
+                          'argparse':{'type':str,'help':'Model Description, used in folder and log naming.'}},
+    # TODO: Will implement
+    # 'mode':              {'default': 'train',
+    #                       'flags':   ['--mode'],
+    #                       'argparse':{'type':str,'choices':['train','test','verify','predict'],'help':'Program mode.  Options include: "train", "test", "verify", "predict"'}},
+    # TODO: Will implement
+    # 'architecture':      {'default': 'basic',
+    #                       'flags':   ['--arch', '-A'],
+    #                       'argparse':{'type':str,
+    #                                   'choices':['basic','res_skip','efficient','parallel_channel','helix','flat_coding','multi_loop','dla'],
+    #                                   'help':'Network Architecture used in the model.  Options include: "basic", "res_skip", "efficient", "parallel_channel", "helix", "flat_coding", "multi_loop","dla"'}},
+    'silent':            {'default': False,
+                          'flags':   ['--silent'],
+                          'argparse':{'action':'store_true','help':'Model training and evaluation runs in silent mode.'}},
+
+
+    'load_model_dir':    {'default': None,
+                          'flags':   ['--load'],
+                          'argparse':{'type':str,'help':'Path of the file the network weights are initialzed with.  If any value is passed, the weights are loaded.'}},
+    'save_model_dir':    {'default': 'models',
+                          'flags':   ['--model_dir'],
+                          'argparse':{'type':str,'help':'Path of the directory the model is saved to.'}},
+    'log_directory':     {'default': 'logs',
+                          'flags':   ['--log_dir'],
+                          'argparse':{'type':str,'help':'Path of the log directory for W&B or TB.'}},
+
+}
+
+def print_configs(configs=None):
+    print("\n-------------------------------------------------------------------------------")
+    print("| {:^75} |".format('Configuration'))
+    print("| {:^23} | {:^23} | {:^23} |".format('Option Descriptions','Option Strings', 'Values Passed In' if configs else 'Default Values'))
+    print("-------------------------------------------------------------------------------")
+    for k,c in default_configs.items():
+        value = c['default']
+        if configs: value = getattr(configs, k)
+        if value is None: value = 'None'
+        if isinstance(value, bool): value = str(value)
+        print("| {:<23} | {:<23} | {:<23} |".format(k, ' , '.join(c['flags']), value))
+        if configs is None and 'help' in c['argparse'].keys(): print("    Help: {} \n".format(c['argparse']['help']))
+    print("-------------------------------------------------------------------------------\n")
+
+def parse_configs():
+    import argparse
+
+    parser = argparse.ArgumentParser(add_help=False)
+    # Programatically add default_configs to parser arguments
+    for k,c in default_configs.items():
+        parser.add_argument(*c['flags'], dest=k, default=c['default'], **c['argparse'])
+    parser.add_argument('-h', '--help', dest='help', action='store_true', default=False)
+    configs = parser.parse_args()
+
+    return configs
+
+### END CODE HERE
--- ant_gridworld.py
+++ ant_gridworld.py
@@ -3,7 +3,9 @@ import numpy as np
 from time import sleep
 import matplotlib.pyplot as plt
 
-# plt.ion()
+from Configure import parse_configs, print_configs
+
+plt.ion()
 
 class Env:
     class State:
@@ -17,13 +19,18 @@ class Env:
         def add_food(self, food_locs, foods):
             for i in range(len(foods)):
                 self.food_space[food_locs[i]] += foods[i]
+
         def add_trail(self, location, weight):
             self.trail_space[location] = weight
+
         def add_obstacle(self, locations):
             for loc in locations:
                 if loc[0]<self.env_size and loc[1]<self.env_size:
                     self.grid_space[loc] = 1
                     self.total_obs_pts.add(loc)
+
+        def remaining_food(self):
+            return np.sum(self.food_space)
 
     def __init__(self,env_size=20, food_num=15, max_wt=5, nest_loc="center", nest_range=1, obstacle_no=10):
         self.reset(env_size, food_num, max_wt, nest_loc, nest_range, obstacle_no)  
@@ -33,6 +40,7 @@ class Env:
         self.done = False
         self.ants = []
         self.expl_ants = []
+        self.antIndex = 0
         self.totalFoodCollected = 0
         if nest_loc == 'center':
             self.nest = ((env_size-1)//2,(env_size-1)//2)
@@ -43,6 +51,15 @@ class Env:
             self.nest = (np.random.choice(np.arange(env_size)),np.random.choice(np.arange(env_size)))
         self.init_food_obstacles(food_num, max_wt, nest_range,obstacle_no)
         self.createColony(self.nest)
+
+    def current_ant(self):
+        if len(self.ants) > self.antIndex:
+            return self.ants[self.antIndex]
+        return None
+
+    def next_ant(self):
+        self.antIndex += 1
+        self.antIndex %= len(self.ants)
 
     def storeFood(self):
         self.totalFoodCollected += 1
@@ -93,28 +110,68 @@ class Env:
             self.expl_ants.append(ant)
         self.ants.append(ant)
 
-    def get_state(self, antID):
+    # def get_state(self, antID):
+    def get_state(self):
+        antID = self.current_ant().antID
         r, c  = self.ants[antID].get()
         convert_input = lambda x : x[1:3]+x[5:6]+x[8:5:-1]+x[3:4]+x[0:1]
-        food = list(np.pad(self.state.food_space,(1,1),constant_values=-1)[r:r+3,c:c+3].flatten())
+        food  = list(np.pad(self.state.food_space, (1,1),constant_values=-1)[r:r+3,c:c+3].flatten())
         trail = list(np.pad(self.state.trail_space,(1,1),constant_values=-1)[r:r+3,c:c+3].flatten())
         return convert_input(food), convert_input(trail)
+
+    # def get_reward(self, state, action):
+    def get_reward(self, action):
+        """ TODO
+        as discussed, assign various rewards for various state,action pairs.
+        """
+        # state = self.get_state()
+        # ant = self.current_ant()
+        return 0
 
     def step(self, action):
         """ TODO depending on action, 
         1. Change food_space. trail_space
         2. Update the location of the ant
-        3. Call get_reward for the action taken
-
-        X. Check if all food is exhausted -> set self.done=True
         """
-        pass
+        reward = self.get_reward(action)
+        if self.antIndex >= len(self.ants) - 1:
+            # trail_len = 15
+            # self.trail_space -= (self.trail_space > 0).astype(np.float32)/trail_len
+            # self.trail_space *= (self.trail_space > 1/trail_len).astype(np.float32)
+            self.state.trail_space -= 0.05         # Pheromone evaporation rate: 0.05 per time step
+            self.state.trail_space[self.state.trail_space<0] = 0
+            self.done = self.state.remaining_food() == 0
 
-    def get_reward(self, state, action):
-        """ TODO
-        as discussed, assign various rewards for various state,action pairs.
-        """
-        pass
+        actions = [ (-1, 0), # North
+                    (-1, 1),
+                    ( 0, 1), # East
+                    ( 1, 1),
+                    ( 1, 0), # South
+                    ( 1,-1),
+                    ( 0,-1), # West
+                    (-1,-1), ]
+
+        set_trail = bool(action//8)
+        pickup_food = bool(action//16)
+        action %= 8
+
+        current_ant = self.current_ant()
+        r,c = current_ant.get()
+        dr, dc = actions[action]
+        nr = min(max(r+dr,0),self.state.grid_space.shape[0]-1)
+        nc = min(max(c+dc,0),self.state.grid_space.shape[0]-1)
+        # current_ant.set((nr,nc))
+        current_ant.location = (nr,nc)
+        if pickup_food and self.state.food_space[nr,nc]:
+            self.state.food_space[nr,nc] -= 1
+        # if set_trail and self.trail_space[self.actors[idx].get()] < 2:    # No upper limit on trail strength
+        if set_trail and (nr,nc) != self.nest:      # No trail at nest
+            self.state.trail_space[nr,nc] += 1            # Pheromone reinforcement
+
+        state = self.get_state()
+        self.next_ant()
+        return state, reward, self.done
+
 
     def plot_environment(self):
         C,R = self.state.grid_space.shape
@@ -140,6 +197,7 @@ class Env:
         plt.scatter(tX,tY, color='#0072BD', s=4)                                    # Trail
         plt.scatter(fX, fY, color='#7E2F8E', s=15)
         plt.scatter(oX, oY, color='#000', s=15)                          # Food particles
+        plt.scatter(*tuple(zip(*[a.get() for a in self.ants])), color='#77AC30', s=30)
         # plt.scatter(antFX, antFY, color='#77AC30', s=30)                            # Ants searching for food
         # plt.scatter(antNFX, antNFY, color='#A2142F', s=30)                          # Ants returning with food
         plt.xticks(range(-1,C,C//10))
@@ -171,6 +229,40 @@ class AntAgent:
     def get(self):
         return self.location
 
+    # TODO: policy will be an object that can be called or function passed into ant, not a method
+    def policy(self, state):
+        # if len(self.memory) == self.memory_size+1:              # TODO: (self.memory) will be a state val
+        #     self.memory.pop(0)
+        # self.memory.append(self.location)
+
+        # food, trail = X
+        # if self.location == self.nest and not self.foraging:    # TODO: (self.location == self.nest) and (self.foraging -> has_food) will be a state vals
+        #     self.store_food()
+        #     self.foraging = True
+        #     self.action_memory = randint(0,7)
+
+        # if self.foraging:
+        #     if self.exploring:
+        #         act = self.explore(food,trail)
+        #     else:
+        #         act = self.forage(food,trail)
+        # # Removing trail from spaces recently visited
+        # else:
+        #     to_nest = self.to_nest()                            # TODO: (to_nest) will be a fuzzy state val
+        #     for v,a in enumerate(self.actions):
+        #         # Remove trails that do not take the ant closer to the nest
+        #         if a not in to_nest:
+        #             trail[v] = 0
+        #     # adding preference for trails closer to home
+        #     trail[list(self.actions).index(to_nest[0])] += 0.5
+        #     act = np.argmax(trail) + 8
+        #         
+        #         
+        # self.action_memory = act % 8
+        # return act
+        return [1/len(self.actions)]*len(self.actions)
+        # return [1]+[0]*(len(self.actions)-1)
+
     def get_action_probabilities(self):
         food, trail = self.env.get_state(self.antID)
         # TODO do neural network step here and get output
@@ -185,24 +277,28 @@ class AntAgent:
         return action
 
     def set(self, action):
-        self.location = [self.location[0] + action[0],self.location[1] + action[1]]
+        self.location = (self.location[0] + action[0],self.location[1] + action[1])
 
 
-def run():
+def run(configs):
     environment = Env()
-    n = 1
-    while not environment.done:
+    state = environment.reset()
+    for _ in range(configs.max_steps):
         for ant in environment.ants:
-            ant.step()
-            environment.plot_environment()
-
-        n+=1
-        if n>5:
-            break
+            choice_dist = ant.policy(state)
+            # print(ant.location,end=' ')
+            action = np.random.choice(list(range(len(choice_dist))),p=choice_dist)
+            # actions.append(action)
+            new_state, reward, done = environment.step(action)
+            # print(action,end=' ')
+            # print(ant.location)
+            state=new_state
+        environment.plot_environment()
 
 
 if __name__=="__main__":
-    run()
+    configs = parse_configs()
+    run(configs)
 
     # print(environment.state.grid_space)
     # environment.plot_environment()
