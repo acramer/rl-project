fea20772bb34388750684e1610fb3b5260775313
TejasAdsul <tejas.adsul1996@gmail.com>
2021-05-02 17:23:43 -0500

    modified decentralized q learning

--- centralized_q.py
+++ centralized_q.py
@@ -89,7 +89,8 @@ class DecentralizedEnvironment(NumpyEnvironment):
         super().__init__(DQAnt,**kwargs)
         self.num_act = 8 
         self.Q = defaultdict(lambda: np.zeros(self.num_act))
-        self.train(epochs,max_steps,epsilon)
+        self.rewards = []
+        # self.train(epochs,max_steps,epsilon)
 
     # TODO: get hyper params epsilon,alpha,gamma
     def train(self,epochs=1,max_steps=600,epsilon=0.7,alpha=0.5,gamma=0.9):
@@ -98,22 +99,20 @@ class DecentralizedEnvironment(NumpyEnvironment):
             ret[np.argmax(self.Q[observation])] = 1 - epsilon + epsilon/len(self.Q[observation])
             return ret
         for _ in range(epochs):
-            done = False
-            max_steps = 1000
-            for i in range(max_steps):
-                for j in range(self.num_ants):
-                    state = (self.ant_locations[j],self.has_food[j])
-                    action = np.random.choice(list(range(self.num_act)),p=epsilon_pi(state)) 
-                    _ , reward, done = self.step(action)
-                    next_state = (self.ant_locations[j],self.has_food[j])
-                    self.Q[state][action] = self.Q[state][action] + alpha*(reward+gamma*max(self.Q[next_state])- self.Q[state][action])
+            total_reward = 0
+            for i in np.arange(self.num_ants):
+                done = False
+                state = (self.ant_locations[i],self.has_food[i])
+                action = np.random.choice(list(range(self.num_act)),p=epsilon_pi(state)) 
+                _ , reward, done = self.step(action)
+                total_reward += reward
+                next_state = (self.ant_locations[i],self.has_food[i])
+                self.Q[state][action] = self.Q[state][action] + alpha*(reward+gamma*max(self.Q[next_state])- self.Q[state][action])
                     # state = next_state
                 if done: break
                 self.state.trail_space = self.state.trail_space-0.01
                 self.state.trail_space[self.state.trail_space<0] = 0
-                self.plot_environment(i)
-            print('Last Step:',i)
-        self.reset()
+            self.rewards.append(total_reward)
 
 class DQAnt(AntAgent):
     def __init__(self,ID,env):
@@ -122,8 +121,8 @@ class DQAnt(AntAgent):
         self.rewards = []
         self.action_memory = 0
 
-    def policy(self,X):
-        # food, trail, obstacles, has_food, action_memory, state_memory, location, to_nest = X[:8],X[8:16],X[16:24],X[24],X[25],X[26:66],tuple(X[66:68]),X[68:]
-        ret = [0]*self.env.num_act
-        ret[np.argmax(self.env.Q[tuple(X)])] = 1
-        return ret
+    # def policy(self,state):
+    #     # food, trail, obstacles, has_food, action_memory, state_memory, location, to_nest = X[:8],X[8:16],X[16:24],X[24],X[25],X[26:66],tuple(X[66:68]),X[68:]
+    #     ret = [epsilon/len(self.Q[state])]*len(self.Q[state])
+    #     ret[np.argmax(self.Q[state])] = 1 - epsilon + epsilon/len(self.Q[state])
+    #     return ret
